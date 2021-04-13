becabf0dec57397d74a430cc23d12d0aec43f522
Steven Xu <51144330+StevenZoo@users.noreply.github.com>
2021-04-12 18:07:15 -0700

    Improve error handling and code clarity

--- pandoc-filters/python/katex.py
+++ pandoc-filters/python/katex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
-import sys
+import logging
 from typing import Optional
 
 from pandocfilters import RawInline, toJSONFilter
@@ -14,6 +14,8 @@ DISPLAY_BYTE = "\x01"
 
 ENCODING = "UTF-8"
 
+logger = logging.getLogger(__name__)
+
 
 # Primary method for Pandoc filter.
 def katex(key, value, format, meta):
@@ -21,50 +23,53 @@ def katex(key, value, format, meta):
         return None
 
     formatter, tex = value
-    display_mode = formatter['t'] == 'DisplayMath'
+    display = formatter['t'] == 'DisplayMath'
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as katex_socket:
-        error = connect(HOST, PORT, katex_socket)
-        if error:
-            log_error(error)
-            return None
+        connect(HOST, PORT, katex_socket)
 
-        html = render(tex, display_mode, katex_socket)
-
+        html = render(tex, display, katex_socket)
         if html is not None:
             return RawInline('html', html)
 
 
-# Sends TeX input to a server that renders into HTML.
-def render(tex: str, display_mode: bool, katex_socket) -> Optional[str]:
-    send_message(tex, display_mode, katex_socket)
+# Renders TeX input to HTML
+def render(tex: str, display: bool, katex_socket) -> Optional[str]:
+    send_message(tex, display, katex_socket)
     data, error_code = get_response(katex_socket)
 
-    if error_code:
-        log_katex_error(tex, data)
+    if error_code > 0:
+        # Log error message with source TeX input
+        logger.error(f'Input: {tex}  {data}')
         return None
 
     return data
 
 
-# Sends a TeX string, prefixed with a flag to set the display mode.
-def send_message(tex: str, display_mode: bool, sock):
-    display_setting = DISPLAY_BYTE if display_mode else INLINE_BYTE
-    input_message = display_setting + tex
-
-    request = input_message.encode(ENCODING)
+# Sends a TeX string, prefixed with the display mode, to the server.
+def send_message(tex: str, display: bool, sock):
+    request = build_request(tex, display)
 
     sock.sendall(request)
     sock.shutdown(socket.SHUT_WR)
 
 
+def build_request(tex: str, display: bool) -> bytearray:
+    display_mode = DISPLAY_BYTE if display else INLINE_BYTE
+    input_message = display_mode + tex
+
+    return input_message.encode(ENCODING)
+
+
 def get_response(sock) -> tuple:
     response = poll(sock)
 
-    # Deconstruct response into error code and rendered HTML/error message, depending on success or error.
+    # Check first byte for error code.
     error_code = response[0]
     data = response[1:].decode(ENCODING)
 
+    # On error, data will be the error message.
+    # On success, data will be the rendered HTML.
     return data, error_code
 
 
@@ -78,19 +83,12 @@ def poll(sock) -> bytearray:
         chunks.extend(chunk)
 
 
-def connect(host: str, port: str, sock) -> int:
+def connect(host: str, port: str, sock):
     try:
         sock.connect((host, port))
     except socket.error as error:
-        return error
-
-
-def log_error(error: str):
-    print(error, file=sys.stderr)
-
-
-def log_katex_error(input: str, error: str):
-    log_error(f'Input: {input}\t{error}')
+        logger.error("Could not connect to Node server.")
+        raise SystemExit(error)
 
 
 if __name__ == '__main__':
