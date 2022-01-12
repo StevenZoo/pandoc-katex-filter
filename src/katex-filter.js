e4509771e28b08a4565127e40649f90c56a9d655
StevenZoo <51144330+StevenZoo@users.noreply.github.com>
2022-01-11 21:53:09 -0800

    Migrate to node filter (#21)
    * Migrate filter from python to node.js
    
    * Update documentation and samples

---
+++ src/katex-filter.js
@@ -0,0 +1,49 @@
+const pandoc = require("pandoc-filter");
+const katex = require("katex");
+
+const BATCH_ERRORS = "batch-katex-errors";
+
+function getMetadataProperty(meta, property) {
+  return meta.hasOwnProperty(property) && meta[property].c;
+}
+
+function logError(message, input) {
+  console.error(message);
+  console.group();
+  console.error("Input:");
+  console.group();
+  console.error(input + "\n");
+  console.groupEnd();
+  console.groupEnd();
+}
+
+function render(tex, options) {
+  try {
+    return [katex.renderToString(tex, options)];
+  } catch (err) {
+    let errOptions = { ...options, throwOnError: false };
+    return [katex.renderToString(tex, errOptions), err];
+  }
+}
+
+function action({ t: type, c: value }, format, meta) {
+  if (type === "Math") {
+    let [attr, tex] = value;
+    let options = { displayMode: attr.t === "DisplayMath", throwOnError: true };
+
+    let [html, err] = render(tex, options);
+
+    // Handle error if needed. 
+    // Either log to stderr or throw immediately.
+    if (err !== undefined) {
+      let shouldBatchErrors = getMetadataProperty(meta, BATCH_ERRORS);
+      if (shouldBatchErrors) logError(err.message, tex);
+      else throw err;
+    }
+
+    // Return HTML generated from KaTeX
+    return pandoc.RawInline("html", html);
+  }
+}
+
+pandoc.stdio(action);
